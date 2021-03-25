b9b7df3c4bf71e1afec09d93e849ea73ec5c62c9
Shiming Zhang <wzshiming@foxmail.com>
2021-03-25 21:46:12 +0800

    Add release macos

---
+++ tools/icns.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+IN=icon/icon
+OUT=release/JumpWay.app/Contents/Resources/JumpWay
+TMP=icons.iconset
+mkdir -p "${TMP}"
+LIST=(16 32 64 128 256 512 1024)
+for SIZE in "${LIST[@]}"; do
+  sips -z "${SIZE}" "${SIZE}" "${IN}.png" -o "${TMP}/icon_${SIZE}x${SIZE}.png"
+done
+
+iconutil -c icns "${TMP}" -o "${OUT}.icns"
+
+rm -rf "${TMP}"
