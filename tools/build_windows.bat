d8f9f466d0faea5246e88710d6c92630fddb78f9
Shiming Zhang <wzshiming@foxmail.com>
2021-03-29 02:02:11 +0800

    Fix build_windows.bat

--- tools/build_windows.bat
+++ tools/build_windows.bat
@@ -2,5 +2,6 @@
 
 mkdir release
 go generate ./webview
+go get github.com/tc-hib/go-winres
 go run github.com/tc-hib/go-winres make --in=tools/windows/winres.json --out=cmd/jumpway/main
 go build -ldflags="-H=windowsgui" -o release/JumpWay.exe ./cmd/jumpway
