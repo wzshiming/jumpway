770187f69610733282668e10d1f3c32353245db5
Shiming Zhang <wzshiming@foxmail.com>
2021-03-29 13:03:16 +0800

    Zip for darwin app

--- tools/build_darwin.sh
+++ tools/build_darwin.sh
@@ -6,3 +6,5 @@ go build -o release/JumpWay.app/Contents/MacOS/JumpWay ./cmd/jumpway
 cp icon/icon.png release/JumpWay.app/Contents/Resources/JumpWay.icns
 ./tools/icns.sh
 cp tools/macos/Info.plist release/JumpWay.app/Contents/
+cd release && zip JumpWay.app.zip -r JumpWay.app
+rm -rf JumpWay.app
